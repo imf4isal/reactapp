3058e6302d0074e3835bdd48895b12d4a0ba9ee0
Faisal <md.faisal1899@yahoo.com>
2022-07-16 19:56:16 +0600

    implemented procedure to catch input values;

--- src/components/Form/SimpleInput.js
+++ src/components/Form/SimpleInput.js
@@ -1,9 +1,27 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameRef = useRef();
+  const enteredNameHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+
+    const enteredValue = nameRef.current.value;
+
+    console.log(enteredName, enteredValue);
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameRef} type='text' id='name' onChange={enteredNameHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
