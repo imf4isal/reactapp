441d214a42de6ec519d336dea08ed84d83ea03c8
Faisal <md.faisal1899@yahoo.com>
2022-07-17 07:32:51 +0600

    added on blur and error functionality

--- src/components/Form/SimpleInput.js
+++ src/components/Form/SimpleInput.js
@@ -2,27 +2,54 @@ import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
 
-  const [enteredName, setEnteredName] = useState('');
-
   const nameRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+    if(enteredName.trim() === ''){
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+    }
+  }
+
   const enteredNameHandler = (event) => {
     setEnteredName(event.target.value);
+    if(enteredName.trim() === ''){
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+    }
   }
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    setEnteredNameTouched(true);
+
+    if(enteredName.trim() === ''){
+      setIsValid(false);
+      return;
+    }
 
     const enteredValue = nameRef.current.value;
 
+    setEnteredName('');
     console.log(enteredName, enteredValue);
   }
 
+  const inputInValid = !isValid && enteredNameTouched;
+  const formControlClass = inputInValid ? 'form-control invalid' : 'form-control';
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className='form-control'>
+      <div className={formControlClass}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameRef} type='text' id='name' onChange={enteredNameHandler} />
+        <input ref={nameRef} type='text' id='name' onChange={enteredNameHandler} onBlur={nameInputBlurHandler} value={enteredName}/>
       </div>
+      {inputInValid && <p className="error-text">Input is not valid.</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
