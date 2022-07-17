07c17ead0933f26d035b23c23ab0908aa7e1f165
Faisal <md.faisal1899@yahoo.com>
2022-07-17 09:15:35 +0600

    finished

--- src/hook/use-input.js
+++ src/hook/use-input.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 const useInput = (validity) => {
-
     const [enteredValue, setEnteredValue] = useState('');
     const [enteredValueTouched, setEnteredValueTouched] = useState(false);
 
